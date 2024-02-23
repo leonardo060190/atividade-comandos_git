fb4db69749437804f7c637730e9f649f4450495b
leonardo060190 <leonardo-morruga@hotmail.com>
2024-02-22 21:12:26 -0300

    ok

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); // Para parsing de JSON
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
